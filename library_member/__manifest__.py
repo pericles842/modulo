5eba26b2737f8691ef4f56dcb7f22dd463b46004
pericles842 <slouis482@mail.com>
2022-02-18 16:12:43 -0400

    entrar

---
+++ library_member/__manifest__.py
@@ -0,0 +1,10 @@
+{
+
+'name': 'Library Members',  
+'description': 'Manage people who will be able to borrow books.',
+'author': 'Louis',
+'depends': ['library_app'],
+'data': [ 'views/book_view.xml'],
+'application': False,
+
+}
