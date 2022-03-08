555e60d6ac9211f50fb2daa60fb2221a620b34f9
pericles842 <slouis482@mail.com>
2022-03-08 16:35:18 -0400

    master

--- form_app/models/form.py
+++ form_app/models/form.py
@@ -1,10 +1,28 @@
-from cgi import FieldStorage
 from dataclasses import field
-import string
-from unicodedata import name
+from datetime import  datetime, date
 from odoo import api, fields, models 
+
 
 class form(models.Model):
     _name = 'form.form'
 
-    name = fields.Char(string='name') #campo
+    name = fields.Char(string='Name', required=True) #campo
+    last_name = fields.Char(string='Last Name')
+    registration_date= fields.Datetime(string='Registration Date',default=fields.Date.today() )
+    ci = fields.Integer(string='C.I')
+    nationality = fields.Selection([('v','V'),
+                                    ('e','E')],'nationality')
+    sexo = fields.Selection([('male','Male'),
+                             ('feminine','Feminine'),
+                             ('other','Other')], string='Sexo')
+    algo = fields.Selection([('civil','Civil'),
+                              ('militar','Militar')], 'Civil/Militar')
+    degree = fields.Char(string='Degree') #grado
+    force = fields.Char(string='Force') #fuerza
+    date_of_birth = fields.Date(string='Date Of Birth',required=True) #Fecha de nacimiento
+    age = fields.Integer(string='Age')
+
+        
+
+
+
