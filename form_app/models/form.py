d9894b46b957e123a145e582c57f9f9dcdb052c5
pericles842 <slouis482@mail.com>
2022-03-09 17:15:29 -0400

    master

--- form_app/models/form.py
+++ form_app/models/form.py
@@ -1,19 +1,19 @@
-from dataclasses import field
 from datetime import  datetime, date
 from email.policy import default
-import string
-from odoo import api, fields, models 
+from odoo import api, fields, models
+from odoo.exceptions import UserError, ValidationError 
 
 
 class form(models.Model):
     _name = 'form.form'
+    
 
     name = fields.Char(string='Name', required=True) #campo
     last_name = fields.Char(string='Last Name')
     registration_date= fields.Datetime(string='Registration Date',default=fields.Date.today() )
-    ci = fields.Integer(string='C.I')
+    ci = fields.Integer(string='C.I', required=True)
     nationality = fields.Selection([('v','V'),
-                                    ('e','E')],'nationality')
+                                    ('e','E')],default="v", string='nationality', readonly='1')
     sexo = fields.Selection([('male','Male'),
                              ('feminine','Feminine'),
                              ('other','Other')], string='Sexo')
@@ -23,6 +23,21 @@ class form(models.Model):
     force = fields.Char(string='Force') #fuerza
     date_of_birth = fields.Date(string='Date Of Birth',required=True) #Fecha de nacimiento
     age = fields.Integer(string='Age')
+
+    @api.onchange('ci')
+    def validate_id(self):
+        lenn =str(self.ci)
+        if len(lenn) != 1:
+            if len(lenn) == 8 or len(lenn) == 7:
+                if  self.ci < 79999999: 
+                    self.nationality = 'v'
+                elif self.ci > 80999999:
+                    self.nationality = 'e'
+            elif len(lenn) > 8 or len(lenn) < 7:
+                raise ValidationError('The ID must contain 8 digits')
+        
+        
+        
 
         
 
