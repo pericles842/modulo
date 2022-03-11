bb95870b21814ab0def519815ac7bdc6c4072bcd
pericles842 <slouis482@mail.com>
2022-03-11 12:35:09 -0400

    master

--- form_app/__manifest__.py
+++ form_app/__manifest__.py
@@ -5,6 +5,8 @@
     'depends':['base'],
     'data':[
         'security/ir.model.access.csv',
+        'data/form.data.xml',
+        'data/form.data.subjets.xml',
         'views/form.form.xml',
         'views/form.career.xml',
         'views/form.subjets.xml'
