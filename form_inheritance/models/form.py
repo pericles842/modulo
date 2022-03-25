d9315fd19d9217702a3728be4bac673fc01d185c
pericles842 <slouis482@mail.com>
2022-03-25 18:26:56 -0400

    master

--- form_inheritance/models/form.py
+++ form_inheritance/models/form.py
@@ -1,3 +1,5 @@
+from datetime import  datetime, date
+import string
 from odoo import api, fields, models
 
 class form(models.Model):
@@ -7,3 +9,5 @@ class form(models.Model):
     gmail = fields.Char(string='Gmail')
     form_list_ids=fields.One2many('form.subjet.list', 'form_subjet_id', string="Name")
     subjet_form_ids=fields.One2many('form.subjet.list', 'subjet_form_id', string="Subjets")
+    current_date=fields.Date(string="Current Date")
+    final_date=fields.Date(string="Final Date")
---
+++ form_inheritance/wizard/view_form_form.py
@@ -0,0 +1,13 @@
+from datetime import  datetime, date
+from odoo import api, fields, models
+
+class form(models.TransietModel ):
+    _name='form.wizard'
+    _inherit = 'form.form'
+
+    current_date=fields.Date(string="Current Date")
+    final_date=fields.Date(string="Final Date") 
+    
+    
+    def button_send(self):
+        return self.current_date
