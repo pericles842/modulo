b3c030d3f4f6a5da0f537bbc22aaca4e65a6b509
pericles842 <slouis482@mail.com>
2022-04-01 10:46:08 -0400

    master

--- form_inheritance/models/form.py
+++ form_inheritance/models/form.py
@@ -1,6 +1,5 @@
-from datetime import  datetime, date
-import string
 from odoo import api, fields, models
+from odoo.exceptions import UserError, ValidationError
 
 class form(models.Model):
     _inherit = 'form.form'
@@ -9,9 +8,18 @@ class form(models.Model):
     gmail = fields.Char(string='Gmail')
     form_list_ids=fields.One2many('form.subjet.list', 'form_subjet_id', string="Name")
     subjet_form_ids=fields.One2many('form.subjet.list', 'subjet_form_id', string="Subjets")
-    current_date=fields.Date(string="Current Date")
-    final_date=fields.Date(string="Final Date")
-    
+    form_wizard_ids = fields.Many2many('form.wizard', string='form_wizard')
+
     @api.model
-    def event_wizard(self):
-        print('que paso zs')
+    def action_wizard(self):
+        act_window_server={
+        "type":"ir.actions.act_window",
+        "name":"Incripcion",
+        "res_model":"form.wizard",
+        "view":"view_form_wizard",
+        "action_id":"view_form_wizard",
+        "view_mode":"form",
+        "contex":"self.ids",
+        "target":"new"
+        }
+        return act_window_server
--- form_inheritance/wizard/view_form_form.py
+++ form_inheritance/wizard/view_form_form.py
@@ -1,8 +1,23 @@
 from datetime import  datetime, date
 from odoo import api, fields, models
+from odoo.exceptions import UserError, ValidationError 
 
 class form(models.TransientModel):
     _name = 'form.wizard'
 
-    current_date=fields.Date(string="Current Date")
-    final_date=fields.Date(string="Final Date")
+    current_date=fields.Date(string="Current Date", default=date.today())
+    final_date=fields.Date(string="Final Date", default=date.today())
+    form_form_ids = fields.Many2many('form.form', string='form.form')
+    
+    @api.model
+    def event_wizard(self,record):
+        print('asdasd')
+
+    @api.onchange('current_date','final_date')
+    def validation_one(self):
+        if self.current_date:
+            if str(self.final_date) < str(self.current_date) or str(self.current_date) > str(self.final_date):    
+                self.current_date = date.today()
+                raise ValidationError('La fecha de inicio no puede ser menor a la fecha final')
+            # elif str(self.final_date)> str(date.today()):
+            #     raise ValidationError('No existen registros de esta fecha :'+ str(self.final_date))
